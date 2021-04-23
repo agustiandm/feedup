96fbcd80db8afb960d89629be3a1a9baf205e0f7
agustiandm <ikhwanagus01@gmail.com>
2021-04-23 19:27:59 +0700

    Initial Commit

---
+++ feedup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  feedup
+//
+//  Created by Agustian DM on 23/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
